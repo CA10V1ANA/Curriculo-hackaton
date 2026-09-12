28c1f61438aa4da42d8b42122a32adb2030b3507
Caio Viana <149689861+CA10V1ANA@users.noreply.github.com>
2026-09-12 14:39:55 -0300

    feat(timeline): histórico profissional
    Desenha a linha do tempo utilizando a nova sintaxe @for do Angular para renderizar a lista.

---
+++ src/app/components/timeline/timeline.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+interface TimelineItem {
+  ano: string;
+  cargo: string;
+  descricao: string;
+}
+
+@Component({
+  selector: 'app-timeline',
+  standalone: false,
+  styleUrl: './timeline.scss',
+  templateUrl: './timeline.html',
+})
+export class Timeline {
+  // Lista de itens que o HTML vai percorrer e desenhar
+  historico: TimelineItem[] = [
+    {
+      ano: '2023 - 2025',
+      cargo: 'Ensino Médio Profissionalizante',
+      descricao: 'Conclusão do Ensino Médio Eeep Marwin.'
+    },
+    {
+      ano: '2025 - Atual',
+      cargo: 'Desenvolvedor Full Stack Estagiário',
+      descricao: 'Atuação em projetos corporativos, entregando valor real para usuários.'
+    },
+    {
+      ano: '2026',
+      cargo: 'Tech Lead',
+      descricao: 'Liderando a equipe no Hackathon Proenergia, arquitetando soluções em Angular.'
+    },
+    {
+      ano: '2026 - Atual',
+      cargo: 'Engenharia de Software - UniAteneu',
+      descricao: 'Aprendizado Sem Limites e desenvolvimento de soluções inovadoras.'
+    }
+  ];
+}
